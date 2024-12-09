2c34b4d65d88ea87b50388eb6b39d8cb19e83f17
kennytobiloba <olaatunbikehinde@gmail.com>
2024-12-09 20:46:17 +0100

    updated

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
-
-
 import {
   RouterProvider,
 } from "react-router-dom";
@@ -16,7 +14,6 @@ createRoot(document.getElementById('root')).render(
      <Provider store={store}>
     <RouterProvider router={router}>
     <Toaster />
-  
     </RouterProvider >
     </Provider>
   
