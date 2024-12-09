93ad99b4f9983f8f703a1031aaa4ba701253566e
kennytobiloba <olaatunbikehinde@gmail.com>
2024-12-09 16:22:35 +0100

    updated

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
-
-
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './router/router.js';
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 
+import './index.css';
+import { store } from './redux/store.js';
+import router from './router/router.js';
+
 createRoot(document.getElementById('root')).render(
-     <Provider store={store}>
-    <RouterProvider router={router}>
-    <Toaster />
-  
-    </RouterProvider >
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster />
     </Provider>
-  
-)
+  </StrictMode>
+);
